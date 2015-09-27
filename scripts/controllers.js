78290ea3fd1d7e04d45b27af0b49e3e39a1c77d1
AndreyK <ponachen@mail.ru>
2015-09-27 21:46:05 +0600

    h 1

---
+++ scripts/controllers.js
@@ -0,0 +1,7 @@
+'use strict';
+angular.module("angularRestfulAuth", [])
+    .controller('HomeCtrl', function($rootScope, $scope)//, ['$rootScope', '$scope', '$location', '$localStorage', 'Main', function($rootScope, $scope, $location, $localStorage, Main)
+{
+	alert('here');
+});	
+//}]);
